7445dbd75ec11786c1bd6a0f8fef592ad8dd5fcc
Will Turner <wturner@hep.ph.liv.ac.uk>
2021-12-10 18:14:40 +0000

    changing map coords

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,16 @@ import geopandas as gpd
 # Welcome to Carbon..!
 Something. 
 """
-
+geojsoninfo = {
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [125.6, 10.1]
+  },
+  "properties": {
+    "name": "Dinagat Islands"
+  }
+}
 
 df = px.data.election()
 geo_df = gpd.GeoDataFrame.from_features(
@@ -14,10 +23,11 @@ geo_df = gpd.GeoDataFrame.from_features(
 ).merge(df, on="district").set_index("district")
 
 fig = px.choropleth_mapbox(geo_df,
-                           geojson=geo_df.geometry,
+                           #geojson=geo_df.geometry,
+                           geojson=geojsoninfo,
                            locations=geo_df.index,
                            color="Joly",
-                           center={"lat": 45.5517, "lon": -73.7073},
+                           center={"lat": 125.6, "lon": 10.1},
                            mapbox_style="open-street-map",
                            zoom=8.5)
 st.plotly_chart(fig)
