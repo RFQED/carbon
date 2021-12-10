fb1cc160aadacdb2d81d95da22a310d875b8479a
Will Turner <wturner@hep.ph.liv.ac.uk>
2021-12-10 18:33:50 +0000

    hope mp token is ok here

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,70 @@
-import streamlit as st
 import plotly.express as px
-import geopandas as gpd
+import pandas as pd
+import json
 
-"""
-# Welcome to Carbon..!
-Something. 
-"""
-geojsoninfo = {
-  "type": "Feature",
-  "geometry": {
-    "type": "Point",
-    "coordinates": [125.6, 10.1]
-  },
-  "properties": {
-    "name": "Dinagat Islands"
-  }
+with open("geojson_suburbs.json") as response:
+    suburbs = json.load(response)
+
+""" geojson_suburbs.json:
+{
+    "type": "FeatureCollection", 
+    "features": [
+        {"type": "Feature", "properties": {"suburb": "a"}, "geometry": {"type": "Polygon", "coordinates": [[[4.7582, 52.2536], [4.7581, 52.2513], [4.7582, 52.2536]]]}, "id": "a"},
+        {"type": "Feature", "properties": {"suburb": "b"}, "geometry": {"type": "Polygon", "coordinates": [[[4.7582, 52.2536], [4.7666, 52.2544], [4.7582, 52.2536]]]}, "id": "b"}
+    ]
 }
+"""
 
-#df = px.data.election()
-#geo_df = gpd.GeoDataFrame.from_features(
-#    px.data.election_geojson()["features"]
-#).merge(df, on="district").set_index("district")
 
-fig = px.choropleth_mapbox(geo_df,
-                           #geojson=geo_df.geometry,
-                           geojson=geojsoninfo,
-                           center={"lat": 125.6, "lon": 10.1},
-                           mapbox_style="open-street-map",
-                           zoom=8.5)
-st.plotly_chart(fig)
+df = pd.DataFrame(data={'suburb': ['a', 'b'], 'phase': ['d', 'e']})
+
+mapbox_token='pk.eyJ1IjoicmZxZWQiLCJhIjoiY2t4MHBxZjE4MHU3NzJ2bnl3cmV6bzZodCJ9.qwxACnMntkPpdmBIa1zzug'
+px.set_mapbox_access_token(mapbox_token)
+
+fig = px.choropleth_mapbox(
+    df, 
+    geojson=suburbs, 
+    locations='suburb',
+    color='phase',
+    color_continuous_scale="Viridis",
+    range_color=(0, 2),
+    zoom=14, 
+    center = {"lat": 52.2540, "lon": 4.7641},
+    opacity=0.2)
+
+fig.show()
+
+#
+#
+#import streamlit as st
+#import plotly.express as px
+#import geopandas as gpd
+#
+#"""
+## Welcome to Carbon..!
+#Something. 
+#"""
+#geojsoninfo = {
+#  "type": "Feature",
+#  "geometry": {
+#    "type": "Point",
+#    "coordinates": [125.6, 10.1]
+#  },
+#  "properties": {
+#    "name": "Dinagat Islands"
+#  }
+#}
+#
+##df = px.data.election()
+##geo_df = gpd.GeoDataFrame.from_features(
+##    px.data.election_geojson()["features"]
+##).merge(df, on="district").set_index("district")
+#
+#fig = px.choropleth_mapbox(geo_df,
+#                           #geojson=geo_df.geometry,
+#                           geojson=geojsoninfo,
+#                           center={"lat": 125.6, "lon": 10.1},
+#                           mapbox_style="open-street-map",
+#                           zoom=8.5)
+#st.plotly_chart(fig)
+
