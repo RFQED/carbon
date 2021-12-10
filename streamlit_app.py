ff46a12b74e2b112ac69cac69ab967b59817b406
Will Turner <wturner@hep.ph.liv.ac.uk>
2021-12-10 22:10:02 +0000

    progress

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,109 +1,33 @@
 import plotly.express as px
 import pandas as pd
 import json
+import geojson
+import geopandas as gpd
 import streamlit as st
+
 st.set_page_config(layout="wide")
 
-nw_long = 55.906
-nw_lat = -4.849
+geojson = gpd.read_file('geojson_suburbs.json')
+df = pd.read_csv('electiondata.csv')
 
-se_long = 55.786
-sw_lat = -4.679
-
-
-
-with open("geojson_suburbs.json") as response:
-    suburbs = json.load(response)
+#mapbox_token='pk.eyJ1IjoicmZxZWQiLCJhIjoiY2t4MHBxZjE4MHU3NzJ2bnl3cmV6bzZodCJ9.qwxACnMntkPpdmBIa1zzug'
+#px.set_mapbox_access_token(mapbox_token)
+plot_variable = st.selectbox(
+     'Variable to be plotted.',
+     ('MeasuredRainFall[cm^3/m^2]', 'Home phone', 'Mobile phone'))
 
 
-#df = pd.DataFrame(data={'suburb': ['a'], 'phase': ['d']})
-#df = pd.DataFrame(data={'suburb': ['a', 'b'], 'phase': ['d', 'e']})
-
-geojson = px.data.election_geojson()
-
-f = open("geojson.json", "a")
-f.write(geojson)
-f.close()
-
-df = px.data.election()
-
-
-mapbox_token='pk.eyJ1IjoicmZxZWQiLCJhIjoiY2t4MHBxZjE4MHU3NzJ2bnl3cmV6bzZodCJ9.qwxACnMntkPpdmBIa1zzug'
-px.set_mapbox_access_token(mapbox_token)
-
-fig = px.choropleth_mapbox(df, geojson=geojson, color="Bergeron",
+fig = px.choropleth_mapbox(df, geojson=geojson, color=plot_variable,
                            locations="district", featureidkey="properties.district",
-                           center={"lat": 45.5517, "lon": -73.7073},
-                           mapbox_style="carto-positron", zoom=9)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-#fig.show()
+                           center={"lat": 55.836, "lon": -4.754},
+                           mapbox_style="carto-positron", zoom=11, opacity=0.4 )
 
-#fig = px.choropleth_mapbox(
-#    df, 
-#    geojson=geojson,
-#    locations='district', featureidkey="properties.district",  
-#    color='phase',
-#    color_continuous_scale="Viridis",
-#    range_color=(0, 2),
-#    zoom=11,
-#    center = {"lat": 52.2540, "lon": 4.7641},
-#    opacity=1)
+fig.update_layout( height=750, margin={"r":0,"t":0,"l":0,"b":0}, mapbox = {
+        'style': "mapbox://styles/rfqed/ckx0prtk02gmq15mty3tlmhpu"},
+    showlegend = False)
 
-
-#fig = px.choropleth_mapbox(
-#    df,
-#    geojson=suburbs,
-#    locations='suburb',
-#    color='phase',
-#    color_continuous_scale="Viridis",
-#    range_color=(0, 2),
-#    zoom=11,
-#    center = {"lat": 52.2540, "lon": 4.7641},
-#    opacity=1)
-
-#fig.update_layout(
-#    mapbox = {
-#        'style': "mapbox://styles/rfqed/ckx0prtk02gmq15mty3tlmhpu"},
-#    showlegend = False)
-
-#fig.update_mapboxes(pitch=45)
-
+fig.update_mapboxes(pitch=45)
+##
 #fig.update_layout(height=750, margin={"r":0,"t":0,"l":0,"b":0})
 
 st.plotly_chart(fig, use_container_width=True)
-
-
-#
-#
-#import streamlit as st
-#import plotly.express as px
-#import geopandas as gpd
-#
-#"""
-## Welcome to Carbon..!
-#Something. 
-#"""
-#geojsoninfo = {
-#  "type": "Feature",
-#  "geometry": {
-#    "type": "Point",
-#    "coordinates": [125.6, 10.1]
-#  },
-#  "properties": {
-#    "name": "Dinagat Islands"
-#  }
-#}
-#
-##df = px.data.election()
-##geo_df = gpd.GeoDataFrame.from_features(
-##    px.data.election_geojson()["features"]
-##).merge(df, on="district").set_index("district")
-#
-#fig = px.choropleth_mapbox(geo_df,
-#                           #geojson=geo_df.geometry,
-#                           geojson=geojsoninfo,
-#                           center={"lat": 125.6, "lon": 10.1},
-#                           mapbox_style="open-street-map",
-#                           zoom=8.5)
-#st.plotly_chart(fig)
-
