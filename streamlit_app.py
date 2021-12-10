9c2ed61be67fb9dfcd51736030785c3a59a0f38b
Will Turner <wturner@hep.ph.liv.ac.uk>
2021-12-10 19:03:20 +0000

    wide

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fig = px.choropleth_mapbox(
 
 fig.update_layout(
     mapbox = {
-        'style': "outdoors"},
+        'style': "carbon"},
     showlegend = False)
 
 fig.update_layout(height=750, margin={"r":0,"t":0,"l":0,"b":0})
